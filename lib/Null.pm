38dcdb3f20fae5cbe5a42c25bd8e3d5950260b75
Nicholas Peter Bamber <nicholas@leonhartsberger.periapt>
2010-09-24 13:48:51 +0100

    ran first tests

---
+++ lib/Null.pm
@@ -0,0 +1,12 @@
+package Null;
+use strict;
+use warnings;
+use Carp;
+
+sub filter {
+    my $text = shift;
+    return $text;
+}
+
+
+1
---
+++ test.pl
@@ -0,0 +1,51 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use Carp;
+use Benchmark qw(cmpthese timethis);
+use lib qw(lib);
+use Readonly;
+use File::Basename;
+use Test::More;
+use Test::Regression;
+use Perl6::Slurp;
+
+use Null;
+
+Readonly my %ROUTINES => (
+    null => \&Null::filter,
+);
+Readonly my $COUNT => 10_000;
+
+my @tests = glob 'in/*';
+
+plan tests => (keys %ROUTINES)*@tests*$COUNT;
+
+#cmpthese -1, {
+#    fast => 'sleep 1',
+#    slow => 'sleep 2',
+#};
+
+foreach my $t (@tests) {
+    compare($t);
+}
+
+#timethis(10, 'sleep 1');
+
+sub compare {
+    my $test = shift;
+    my $basename = basename($test);
+
+    print "**********${basename}*************\n";
+    foreach my $key (keys %ROUTINES) {
+        timethis($COUNT, sub {
+            my $input = slurp $test;
+            ok_regression(
+                sub {return &{$ROUTINES{$key}}($input)},
+                "out/$basename",
+                "$basename - $key"
+            );
+        });               
+    }
+}
